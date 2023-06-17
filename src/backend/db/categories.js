d82608d854717d73ccf81039e3e972727d1cd66e
yashwith26 <srysmk483@gmail.com>
2023-06-17 16:01:33 +0530

    updated products

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -10,18 +10,18 @@ export const categories = [
     _id: uuid(),
     categoryName: "Men",
     description: "",
-    image: "assets/images/men-category-img.jpg",
+    image: "https://assets.ajio.com/medias/sys_master/root/20230418/eRdu/643e955a711cf97ba72fba0a/-473Wx593H-466070858-black-MODEL2.jpg",
   },
   {
     _id: uuid(),
     categoryName: "Women",
     description: "",
-    image: "assets/images/women-category-img.jpg",
+    image: "https://assets.ajio.com/medias/sys_master/root/20230321/NWTC/6418dd6cf997dde6f40fd064/-473Wx593H-465679345-navy-MODEL.jpg",
   },
   {
     _id: uuid(),
     categoryName: "Kids",
     description: "",
-    image: "assets/images/kids-category-img.jpg",
+    image: "https://assets.ajio.com/medias/sys_master/root/20230221/rGBf/63f475aaf997dde6f4b5ea07/-473Wx593H-463873437-multi-MODEL.jpg",
   },
 ];
